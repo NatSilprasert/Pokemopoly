0b83257f7d5dc48556875ef79a58036858b31aa8
RyuJomphol <jomphol2548@gmail.com>
2025-12-06 20:02:51 +0700

    Potion Super Potion Repel Revive

---
+++ src/main/java/com/pokemopoly/cards/items/Rare_Candy.java
@@ -0,0 +1,59 @@
+package com.pokemopoly.cards.items;
+
+import com.pokemopoly.Game;
+import com.pokemopoly.cards.ItemCard;
+import com.pokemopoly.cards.PokemonCard;
+import com.pokemopoly.cards.pokemon.interfaces.Evolvable;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Rare_Candy extends ItemCard {
+    public Rare_Candy() {
+        super("I017",
+                "Rare Candy",
+                "Choose 1 Pokémon to evolve!");
+    }
+    @Override
+    public void activate(Game game) {
+        List<PokemonCard> evolvablePokemons = game.getCurrentPlayer().getTeam().stream()
+                .filter(p -> p instanceof Evolvable)
+                .toList();
+
+                        if (!evolvablePokemons.isEmpty()) {
+            System.out.println("Choose pokemon to evolve!");
+            for (int i = 0; i < evolvablePokemons.size(); i++) {
+                System.out.println((i + 1) + ". " + evolvablePokemons.get(i).getName());
+            }
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        int choice = -1;
+
+                        while (true) {
+            if (scanner.hasNextInt()) {
+                choice = scanner.nextInt();
+                if (choice >= 1 && choice <= evolvablePokemons.size()) {
+                    break;
+                } else {
+                    System.out.println("❌ Invalid number. Please select between 1 and " + evolvablePokemons.size() + "!");
+                }
+            } else {
+                System.out.println("❌ Please enter a valid number!");
+                scanner.next();
+            }
+        }
+
+        PokemonCard selected = evolvablePokemons.get(choice - 1);
+                        System.out.println("You chose " + selected.getName() + " to evolve!");
+
+        Evolvable evolvable = (Evolvable) selected;
+        PokemonCard evolvedPokemon = evolvable.evolve();
+
+        int index = game.getCurrentPlayer().getTeam().indexOf(selected);
+        if (index != -1) {
+            game.getCurrentPlayer().getTeam().set(index, evolvedPokemon);
+        }
+        System.out.println("It has evolved to " + selected.getName() + "!");
+    }
+}
--- src/main/java/com/pokemopoly/cards/items/Revive.java
+++ src/main/java/com/pokemopoly/cards/items/Revive.java
@@ -2,14 +2,66 @@ package com.pokemopoly.cards.items;
 
 import com.pokemopoly.Game;
 import com.pokemopoly.cards.ItemCard;
+import com.pokemopoly.cards.PokemonCard;
+import com.pokemopoly.player.Player;
+
+import java.util.Scanner;
 
 public class Revive extends ItemCard {
 
-    public Revive(String id, String name, String description) {
-        super(id, name, description);
+    public Revive() {
+        super("I019", "Revive",
+                "Revives a fainted Pokémon to 50% HP.");
     }
 
+    @Override
     public void activate(Game game) {
+        Player player = game.getCurrentPlayer();
+        Scanner scanner = new Scanner(System.in);
 
-    };
+        // Find fainted Pokémon
+        System.out.println("🧪 Using Revive...");
+        System.out.println("Checking your team...");
+
+        // List all fainted Pokémon
+        int count = 0;
+        for (PokemonCard p : player.getTeam()) {
+            if (!p.isAlive()) count++;
+        }
+
+        if (count == 0) {
+            System.out.println("❌ No fainted Pokémon to revive!");
+            return;
+        }
+
+        System.out.println("Choose a Pokémon to revive:");
+        for (int i = 0; i < player.getTeam().size(); i++) {
+            PokemonCard p = player.getTeam().get(i);
+            if (!p.isAlive()) {
+                System.out.println((i + 1) + ". " + p.getName() + " (Fainted)");
+            }
+        }
+
+        int choice;
+        while (true) {
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt() - 1;
+
+            if (choice >= 0 && choice < player.getTeam().size()
+                    && !player.getTeam().get(choice).isAlive()) {
+                break;
+            }
+            System.out.println("Invalid choice. Select a fainted Pokémon only.");
+        }
+
+        PokemonCard selected = player.getTeam().get(choice);
+
+        // Revive logic
+        int reviveHP = (selected.getMaxHp() + 1) / 2; // 50% rounded up
+        selected.setAlive(true);
+        selected.setHp(reviveHP);
+
+        System.out.println("✨ " + selected.getName()
+                + " has been revived with " + reviveHP + " HP!");
+    }
 }
